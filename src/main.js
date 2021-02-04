248fd90fc5bf4be2216e30f32dfdb79469e7f1b1
NaoOtosaka <zixuanJIMO9703>
2021-02-04 11:00:46 +0800

    服务侧地址更正

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ Vue.prototype.axios = axios
 Vue.prototype.echarts = echarts
 
 
-axios.defaults.baseURL = 'http://localhost:9222';
+axios.defaults.baseURL = 'http://10.219.152.171:9222';
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 axios.defaults.timeout = 10000;
 axios.defaults.withCredentials = true;
