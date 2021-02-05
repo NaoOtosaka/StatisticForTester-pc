36a846a4ad7efd8e48c7c2f9c99cfa656913667f
NaoOtosaka <zixuanJIMO9703>
2021-02-05 13:58:38 +0800

    正式环境接口域名配置

--- src/main.js
+++ src/main.js
@@ -14,7 +14,11 @@ Vue.prototype.axios = axios
 Vue.prototype.echarts = echarts
 
 // axios全局设置
+// debug
 axios.defaults.baseURL = 'http://10.219.152.171:9222';
+// prod
+axios.defaults.baseURL = 'http://10.219.152.253:9223';
+
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 axios.defaults.timeout = 10000;
 axios.defaults.withCredentials = true;
