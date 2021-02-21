208816fbf4d9aa81ab7ec772fc0d759c6bc212ec
NaoOtosaka <zixuanJIMO9703>
2021-02-21 15:12:33 +0800

    进度展示

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,18 @@
 <!--仪表盘主框架-->
 <template>
-  <div>
-    这是一个空的首页
+  <div style="text-align: left">
+    <h2>仪表盘内容待填充<br></h2>
+    <hr>
+    已完成：<br>
+     - 主体框架<br>
+     - 基础数据列表（项目/开发/测试/BUG）<br>
+     - 基础数据展示（项目/测试/BUG）<br>
+     - 项目详情页基础展示<br>
+    <hr>
+    待制作：<br>
+     - 数据图表填充<br>
+     - 项目详情页数据展示<br>
+     - ...
   </div>
 <!--  <bug :data="bug_data"/>-->
 </template>
