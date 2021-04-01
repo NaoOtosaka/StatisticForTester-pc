7408ae5331e294ac9e8d7ee1896989c31bab2be3
NaoOtosaka <zixuanJIMO9703>
2021-04-01 15:47:02 +0800

    进度更新

--- src/components/chart/bug/BugCategoryCountWithProject.vue
+++ src/components/chart/bug/BugCategoryCountWithProject.vue
@@ -53,6 +53,19 @@ export default {
           left: 'center',
           text: '项目BUG分类占比',
         },
+        toolbox: {
+          show: true,
+          orient: 'vertical',
+          left: '3%',
+          top: 'top',
+          feature: {
+            mark: {show: true},
+            dataView: {show: true, readOnly: false},
+            // magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+          }
+        },
         tooltip: {
           trigger: 'item'
         },
